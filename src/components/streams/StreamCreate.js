3b6618af4ae5bd1ab100d6441f09f56af6dae896
Chaiwat Kanchanarat <Chaiwat_K@banpu.co.th>
2018-12-26 17:29:44 +0700

    Upto lecture 230

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return (
-        <div>StreamCreate</div>
-    );
+class StreamCreate extends React.Component {
+    renderInput(formProps){
+        const { input, label, meta } = formProps;
+        console.log(meta);
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+                <div className="ui text red">{meta.error}</div>
+            </div>
+        );
+    }
+
+    onSubmit(formValues){
+        console.log(formValues);
+    }
+
+    render(){   
+        console.log(this.props);     
+        return (
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name="title" component={this.renderInput} label="Enter title: " />
+                <Field name="description" component={this.renderInput} label="Enter descripiton: " />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
 };
 
-export default StreamCreate;
+const validate = (formValues) => {
+    const errors = {};
+    if(!formValues.title){
+        // only ran if user did not enter title
+        errors.title = 'You must enter a title';
+    }
+    if(!formValues.description){
+        errors.description = 'You must enter a description';
+    }
+    return errors;
+};
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate
+})(StreamCreate);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import env from './config/env';
 import App from './components/App';
+import reducers from './reducers';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducers,
+    composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
-    <App env={env} />,
+    <Provider store={store}>
+        <App env={env} />
+    </Provider>,
     document.querySelector('#root')
 );
